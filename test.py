ea6bf516299319321fa4cdf22f9156633579e525
Kevin Micheal Babise <babskevon@gmail.com>
2022-11-29 12:05:03 +0300

    last update

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import requests
 import json
 from get_images import get_image_height
-url = 'http://127.0.0.1:8000/photo/'
+url = 'https://farmbotug.pythonanywhere.com/photo/'
+# url = 'http://127.0.0.1:8000/photo/'
 name = get_image_height(0)
 # myfiles = {'file': open(name ,'rb')}
 f = open(name,'rb')
 response = requests.post(url,files={'name':f})
-response = json.loads(response.text)
+# response = json.loads(response.text)
+response = response.text
 print(response)
-# {'prediction': 'bean_rust', 'score': 99.61293935775757}
-# {'prediction': 'water_stressed', 'score': 51.284801959991455}
+# {'stress': 'water_stressed', 'stress_score': 99.89928603172302, 'bean': 'bean_rust', 'bean_score': 86.40068769454956}
+
+
+# c166dc5464c154f7b30909e1783243f4cb7a96a4
